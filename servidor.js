db7923b45d144e17d7bafca808b5b903e29ba5d9
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-09-21 19:33:21 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -3,14 +3,14 @@ const loginRoutes = require('./Routes/loginRoute');
 const cadastroRoutes = require('./Routes/cadastroRoute');
 
 const app = express();
-const PORT = 3000;
+// Configuração corrigida para aceitar a porta dinâmica do Render
+const PORT = process.env.PORT || 3000;
 
 // Configurações para ler JSON e exibir o Frontend
 app.use(express.json());
 app.use(express.static('Front-end'));
 
 // Engata o arquivo de rotas com o prefixo '/api'
-// Substitua o app.use('/api', authRoutes) por estes dois:
 app.use('/api', loginRoutes);
 app.use('/api', cadastroRoutes);
 

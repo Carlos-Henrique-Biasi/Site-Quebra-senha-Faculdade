0f4e04e639628e05dfd66224c9268db693571e19
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-09-22 09:55:24 -0300

    banco de dados local

--- Data/data.js
+++ Data/data.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2/promise');
+require('dotenv').config();
+const mysql = require('mysql2');
 
-// Cria um pool de conexões com o banco de dados
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'SuaSenhaDoMySQLAqui', 
-    database: 'projeto_faculdade',
+const conexao = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 100, // Permite até 10 requisições simultâneas
+    connectionLimit: 10,
     queueLimit: 0
 });
 
-module.exports = pool;
+module.exports = conexao.promise();
